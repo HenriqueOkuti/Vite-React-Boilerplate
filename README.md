9e2db9f2fa0a2f9960f1642b5ae299b4691e2b0c
Henrique Okuti <henrique.krastins@gmail.com>
2023-08-24 12:53:54 -0300

    Boilerplate defined

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ad Manager
+
+(To be added)
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='App'>
+      <h1>Start developing here</h1>
+    </div>
+  );
+}
+
+export default App;
